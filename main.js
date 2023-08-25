8430012729ae12bda6afd1a4bcd9f1335f79475f
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-25 00:03:45 -0700

    add

---
+++ main.js
@@ -0,0 +1,23 @@
+//classes
+
+class Flowers{
+	
+	constructor(name, color){
+		
+		this.name = name;
+		
+		this.color = color;
+		
+	}
+	
+}
+
+const flower1=new Flowers("lotus", "yellow");
+
+const flower2=new Flowers("rose", "red");
+
+
+console.log(flower1.name);
+
+console.log(flower2.name);
+		
